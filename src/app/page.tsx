a42c12f7667bb8a7d57661d2a6a07beb52ef6ca8
shaheenj <jawadishaheen@gmail.com>
2024-10-04 19:26:51 +0100

    Refactor page component and add dynamic import for Orrery component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
- 
+ "use client";
 import styles from "./page.module.css";
+import dynamic from 'next/dynamic';
+ 
+const Orrery = dynamic(() => import('../components/main'), { ssr: false });
+
 
 export default function Home() {
   return (
-    <div  >
-      Hello NASA 
- 
+    <div  > 
+      <Orrery />
     </div>
   );
 }
