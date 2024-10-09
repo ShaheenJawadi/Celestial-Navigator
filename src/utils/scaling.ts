7d8bfeb82a313d8ac901c89e979d225db632e3c9
shaheenj <jawadishaheen@gmail.com>
2024-10-09 03:17:10 +0100

    Refactor sun scaling constant

--- src/utils/scaling.ts
+++ src/utils/scaling.ts
@@ -1,4 +1,4 @@
-export const SUN_SCALE_FACTOR = 1 / 30000;   
+export const SUN_SCALE_FACTOR = 1 / 40000;   
 export const PLANET_SIZE_SCALE_FACTOR = 1 / 2000;
 
 export const DISTANCE_SCALE_FACTOR = 100; 
