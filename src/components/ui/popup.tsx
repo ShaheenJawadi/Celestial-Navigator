c0becad52e6b70073ed50fd990043a8a67515655
shaheenj <jawadishaheen@gmail.com>
2024-10-10 20:55:55 +0100

    Refactor popup component styles and add popup header

--- src/components/ui/popup.tsx
+++ src/components/ui/popup.tsx
@@ -1,7 +1,15 @@
+import Icon from "@mdi/react";
+import { mdiArrowLeftThick } from "@mdi/js";
+
 export const Popup = () => {
   return (
     <div className="popupHolder">
-      <div className="popup"></div>
+      <div className="popup">
+        <div className="headSection">
+          <Icon path={mdiArrowLeftThick} size={1} />
+          <h2 className="identifier">astroid</h2>
+        </div>
+      </div>
     </div>
   );
 };
