f89f45f77296b7967db1d72147c6af983015d6e0
shaheenj <jawadishaheen@gmail.com>
2024-10-11 00:45:06 +0100

    Refactor planet.ts and planetsInformation.ts to add planet information

---
+++ src/data/planetsInformation.ts
@@ -0,0 +1,101 @@
+import { planetInfoType } from "@/types/planet";
+
+
+
+export const planetsInformation: planetInfoType = 
+ 
+    {
+        "Earth": [
+            {
+                "title": "Mass",
+                "us": {
+                    "value": 5.97219,
+                    "unit": "×10^24 kg"
+                },
+                "metric": {
+                    "value": 5.97219,
+                    "unit": "×10^24 kg"
+                }
+            },
+            {
+                "title": "Volume",
+                "us": {
+                    "value": 1.08321,
+                    "unit": "×10^12 km³"
+                },
+                "metric": {
+                    "value": 1.08321,
+                    "unit": "×10^12 km³"
+                }
+            },
+            {
+                "title": "Radius",
+                "us": {
+                    "value": 6371,
+                    "unit": "km"
+                },
+                "metric": {
+                    "value": 6371,
+                    "unit": "km"
+                }
+            },
+            {
+                "title": "Surface Area",
+                "us": {
+                    "value": 510072000,
+                    "unit": "km²"
+                },
+                "metric": {
+                    "value": 510072000,
+                    "unit": "km²"
+                }
+            },
+            {
+                "title": "Gravity",
+                "us": {
+                    "value": 9.8,
+                    "unit": "m/s²"
+                },
+                "metric": {
+                    "value": 9.8,
+                    "unit": "m/s²"
+                }
+            },
+            {
+                "title": "Length of Day",
+                "us": {
+                    "value": 24,
+                    "unit": "hours"
+                },
+                "metric": {
+                    "value": 24,
+                    "unit": "hours"
+                }
+            },
+            {
+                "title": "Distance from Sun",
+                "us": {
+                    "value": 149598262,
+                    "unit": "km"
+                },
+                "metric": {
+                    "value": 149598262,
+                    "unit": "km"
+                }
+            },
+            {
+                "title": "Length of Year",
+                "us": {
+                    "value": 365.24,
+                    "unit": "days"
+                },
+                "metric": {
+                    "value": 365.24,
+                    "unit": "days"
+                }
+            },
+        ]  
+    }
+
+
+ 
--- src/types/planet.ts
+++ src/types/planet.ts
@@ -2,7 +2,7 @@
 export type planetType = {
     name: string;
     texture: string;
-    color : string;
+    color: string;
     radius: number;
 
     keplerianElements: keplerianElementsType
@@ -15,4 +15,19 @@ export type keplerianElementsType = {
     L: number; // mean longitude
     longPeri: number; // longitude of perihelion
     longNode: number; // longitude of ascending node
+}
+
+
+export type planetInfoType = {
+    [planetName: string]: {
+        title: string;
+        us: {
+            value: number;
+            unit: string;
+        }
+        metric: {
+            value: number;
+            unit: string;
+        }
+    }[]
 }
