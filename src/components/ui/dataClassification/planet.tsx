08ab5dbf051b9ee21024b80e1783323c98b00f64
shaheenj <jawadishaheen@gmail.com>
2024-10-11 21:07:15 +0100

    Refactor planet information popup

--- src/components/ui/dataClassification/planet.tsx
+++ src/components/ui/dataClassification/planet.tsx
@@ -4,50 +4,75 @@ import React from "react";
 
 const displayData = ({ planetName }: { planetName: string }) => {
   const currentPlanet: PlanetProperty[] = planetsInformation[planetName];
-  const [unitSystem, setUnitSystem] = React.useState<"metric"|"us">("us");
+  const [unitSystem, setUnitSystem] = React.useState<"metric" | "us">("us");
 
   return (
     <>
-    <div className="units">
-        <button  className={unitSystem=="us" ? "selected" : ""}  onClick={() => setUnitSystem("us")}>US</button>
-        <button  className={unitSystem=="metric" ? "selected" : ""} onClick={() => setUnitSystem("metric")}>Metric</button>
-    </div>
-    <div className="dataBox">
+      <div className="units">
+        <button
+          className={unitSystem == "us" ? "selected" : ""}
+          onClick={() => setUnitSystem("us")}
+        >
+          US
+        </button>
+        <button
+          className={unitSystem == "metric" ? "selected" : ""}
+          onClick={() => setUnitSystem("metric")}
+        >
+          Metric
+        </button>
+      </div>
+      <div className="dataBox">
+        {currentPlanet.map((planet, index) => {
+          return (
+            <div key={index} className="single">
+              <h4>
+                {planet.label}: {"  "}
+              </h4>
+              <span>
+                {planet[unitSystem].value}{" "}
+                <DisplayUnit unit={planet[unitSystem].unit as string} />{" "}
+              </span>
+            </div>
+          );
+        })}
 
-      {currentPlanet.map((planet, index) => {
-        return (
-          <div key={index} className="single">
-            <h4>
-              {planet.label}: {"  "}
-            </h4>
-            <span>
-              {planet[unitSystem].value}{" "}
-              <DisplayUnit unit={planet[unitSystem].unit as string} />{" "}
-            </span>
-          </div>
-        );
-      })}
-      <div style={{marginTop:10}} className="single">
-            <h4>
-              source:
-            </h4>
-            <span>
-              <a href="https://nssdc.gsfc.nasa.gov/planetary/factsheet/">NASA Planetary Fact Sheet - Metric</a>
-            </span>
-          </div>
-          </div>
+<div style={{ marginTop: 10 }} className="single">
+          <h4>* - See the</h4>
+          <span>
+          <a rel="noopener noreferrer" target="_blank"  href="https://nssdc.gsfc.nasa.gov/planetary/factsheet/planetfact_notes.html">
+          Fact Sheet Notes.
+              </a>
+          </span>
+        </div>
+
+        <div style={{ marginTop: 10 }} className="single">
+          <h4>source:</h4>
+          <span>
+            {unitSystem == "us" ? (
+              <a rel="noopener noreferrer" target="_blank" href="https://nssdc.gsfc.nasa.gov/planetary/factsheet/planet_table_british.html">
+                NASA Planetary Fact Sheet - U.S. Units
+              </a>
+            ) : (
+              <a rel="noopener noreferrer" target="_blank"  href="https://nssdc.gsfc.nasa.gov/planetary/factsheet/">
+                NASA Planetary Fact Sheet - Metric
+              </a>
+            )}
+          </span>
+        </div>
+      </div>
     </>
   );
 };
 
 const DisplayUnit = ({ unit }: { unit: string }) => {
-    let addMultiple = " ";
+  let addMultiple = " ";
   if (unit.startsWith("10")) {
-    addMultiple=" x ";
-  }  
+    addMultiple = " x ";
+  }
   return (
     <span>
-      {addMultiple} 
+      {addMultiple}
       <span dangerouslySetInnerHTML={{ __html: unit }} />{" "}
     </span>
   );
