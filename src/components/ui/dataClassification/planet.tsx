91f99a5b7243cb9a6631ff1569b4c26e57b02c54
shaheenj <jawadishaheen@gmail.com>
2024-10-11 20:39:55 +0100

    Refactor planet data display and add unit multiplier

--- src/components/ui/dataClassification/planet.tsx
+++ src/components/ui/dataClassification/planet.tsx
@@ -2,19 +2,37 @@ import { planetsInformation } from "@/data/planetsInformation";
 import { PlanetProperty } from "@/types/planet";
 
 const displayData = ({ planetName }: { planetName: string }) => {
-  const currentPlanet:PlanetProperty[] = planetsInformation[planetName];
- 
+  const currentPlanet: PlanetProperty[] = planetsInformation[planetName];
+
   return (
     <>
-      {currentPlanet.map((planet , index) => {
+      {currentPlanet.map((planet, index) => {
         return (
           <div key={index} className="single">
-            <h4>{planet.label}: {"  "}</h4>
-            <span>{planet.metric.value}  <span dangerouslySetInnerHTML={{ __html: planet.metric.unit }} />   </span>
+            <h4>
+              {planet.label}: {"  "}
+            </h4>
+            <span>
+              {planet.metric.value}{" "}
+              <DisplayUnit unit={planet.metric.unit as string} />{" "}
+            </span>
           </div>
         );
       })}
     </>
+  );
+};
+
+const DisplayUnit = ({ unit }: { unit: string }) => {
+    let addMultiple = "";
+  if (unit.startsWith("10")) {
+    addMultiple="*";
+  }  
+  return (
+    <span>
+      {addMultiple} 
+      <span dangerouslySetInnerHTML={{ __html: unit }} />{" "}
+    </span>
   );
 };
 
