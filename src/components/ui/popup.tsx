695eedbc2e84e2fbdf83b749dc63d25b5f3f2f04
shaheenj <jawadishaheen@gmail.com>
2024-10-11 19:12:48 +0100

    Add close button functionality in popup

--- src/components/ui/popup.tsx
+++ src/components/ui/popup.tsx
@@ -1,19 +1,24 @@
 "use client";
 import Icon from "@mdi/react";
 import { mdiArrowLeftThick } from "@mdi/js";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store";
- 
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store";
+import { closePopup } from "@/store/generalState";
 
 export const Popup = () => {
   const state = useSelector((state: RootState) => state.generalState);
+
+  const dispatch = useDispatch<AppDispatch>()
+  
   return (
     <>
       {!state.isPopupOpen ? null : (
         <div className="popupHolder">
           <div className="popup">
             <div className="headSection">
-              <Icon path={mdiArrowLeftThick} size={1} />
+              <div className="close icon" onClick={() => dispatch(closePopup())}>
+                <Icon path={mdiArrowLeftThick} size={1} />
+              </div>
               <h2 className="identifier">astroid</h2>
             </div>
 
