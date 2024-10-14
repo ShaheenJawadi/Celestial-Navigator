c1560351e8ef89087b5a54ca6ef73a751044692b
shaheenj <jawadishaheen@gmail.com>
2024-10-14 05:17:20 +0100

    Refactor CSS to improve popup styling and add fun facts to sun component

--- src/components/ui/dataClassification/sun.tsx
+++ src/components/ui/dataClassification/sun.tsx
@@ -5,6 +5,25 @@ import { sunInfo } from "@/data/sunInformations";
 const DisplayData = () => {
   return (
     <>
+
+<div className="subDataSep">
+              <div className="tit">Fun Facts</div>
+              <div className="separator"></div>
+            </div>
+      <div className="funFacts">
+        <ul>
+          <li>
+            The Sun is essentially a giant nuclear fusion reactor. Every second,
+            it fuses 600 million tons of hydrogen into helium, producing the
+            light and heat that powers life on Earth!
+          </li>
+          <li>
+            The Sun is so massive that it contains 99.86% of the total mass of
+            the entire Solar System! It could fit around 1.3 million Earths
+            inside it.
+          </li>
+        </ul>
+      </div>
       <div className="units">
         <small>*Only metric values are provided</small>
       </div>
@@ -16,7 +35,7 @@ const DisplayData = () => {
               <div className="tit">{key}</div>
               <div className="separator"></div>
             </div>
-            {sunInfo[key]?.data.map((planet, index) => {
+            {sunInfo[key]?.data?.map((planet, index) => {
               return (
                 <div key={index} className="single">
                   <h4>
