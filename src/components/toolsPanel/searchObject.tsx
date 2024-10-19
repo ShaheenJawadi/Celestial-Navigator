de642bee184ae76bcf2f0c87f1b5c16dfc64a40e
shaheenj <jawadishaheen@gmail.com>
2024-10-19 07:21:16 +0100

    Refactor search form styling and functionality

--- src/components/toolsPanel/searchObject.tsx
+++ src/components/toolsPanel/searchObject.tsx
@@ -1,8 +1,35 @@
+import { mdiCardSearchOutline, mdiMagnify } from "@mdi/js";
+import Icon from "@mdi/react";
+import { SetStateAction, useState } from "react";
+
 const SearchObject = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+      setSearchTerm(e.target.value);
+    };
+  
+    const handleSearch = (e: { preventDefault: () => void; }) => {
+      e.preventDefault();
+        console.log(searchTerm);
+    };
     return (
-      <div>
-        <h1>SearchObject</h1>
-      </div>
+        <>
+        <form className={"searchForm"} onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search for an object"
+          value={searchTerm}
+          onChange={handleInputChange}
+          className={'searchInput'}
+        />
+        <button type="submit" className={"searchButton"}> 
+          <Icon path={mdiMagnify} size={1} />
+        </button>
+        
+      </form>
+      <small>*Search for planet and NEO by Designation or SPK-ID or Object full name</small>
+      </>
     );
   };
   export default SearchObject;
