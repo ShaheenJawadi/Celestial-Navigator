732a89ef52b54738251d02ea3439835e69d6633f
shaheenj <jawadishaheen@gmail.com>
2024-10-21 10:49:21 +0100

    Refactor UI scaling panel

--- src/components/ui/displayScaling.tsx
+++ src/components/ui/displayScaling.tsx
@@ -1,40 +1,49 @@
-import { div } from "three/webgpu";
+import { AU_TO_KM, convertUSToMetricDistances } from "@/utils/conversionHelpers";
+import { useState } from "react";
 
 const DisplayScaling = () => {
+    const SunInitialPixelScale = 149597370;
+    const PlanetInitialPixelScale = 299196390;
+
+  const [sunScaling, setSunScaling] = useState(149597370);
+  const [planetScaling, setPlanetScaling] = useState(299196390);
+  const [distanceScaling, setDistanceScaling] = useState(AU_TO_KM);
+
+
   return (
     <div className="scalingPanel">
       <table>
         <tr className="single">
-          <td> 
+          <td>
             <div className="label"> Sun scaling</div>
           </td>
-          <td> 
+          <td>
             <div className="scalingLine"></div>
           </td>
-          <td> 
-            <div className="value">149,597,370 km</div>
+          <td>
+            <div className="value">{convertUSToMetricDistances(sunScaling , "metric" , "metric").st}</div>
           </td>
         </tr>
         <tr className="single">
-          <td> 
+          <td>
             <div className="label"> Planets scaling</div>
           </td>
-          <td> 
+          <td>
             <div className="scalingLine"></div>
           </td>
-          <td> 
-            <div className="value">299,196,390 km</div>
+          <td>
+            <div className="value">{convertUSToMetricDistances(planetScaling , "metric" , "metric").st}</div>
           </td>
         </tr>
         <tr className="single">
-          <td> 
+          <td>
             <div className="label"> Distances scaling</div>
           </td>
-          <td> 
+          <td>
             <div className="scalingLine"></div>
           </td>
-          <td> 
-            <div className="value">1AU</div>
+          <td>
+            <div className="value">{convertUSToMetricDistances(distanceScaling , "metric" , "metric").st}</div>
           </td>
         </tr>
       </table>
