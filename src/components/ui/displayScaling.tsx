623ae1b9874f27ecebc0a82da806dba07b593da6
shaheenj <jawadishaheen@gmail.com>
2024-10-21 09:22:33 +0100

    Refactor UI scaling panel and add display scaling functionality

--- src/components/ui/displayScaling.tsx
+++ src/components/ui/displayScaling.tsx
@@ -1,7 +1,31 @@
 import { div } from "three/webgpu";
 
 const DisplayScaling = () => {
-  return (<div> scale</div>);
+  return (
+
+    <div className="scalingPanel">
+
+      <div className="single">
+        <div className="label"> Sun Scaling</div>
+        <div className="scalingLine" ></div>
+        <div className="value" >149,597,370 km</div>
+
+      </div>
+      <div className="single">
+        <div className="label">Distances scaling </div>
+        <div className="scalingLine" ></div>
+        <div className="value" >1AU</div>
+
+      </div>
+
+      <div className="single">
+        <div className="label">Planets scaling</div>
+        <div className="scalingLine" ></div>
+        <div className="value" >299,196,390 km</div>
+
+      </div>
+    </div>
+  );
 };
 
 export default DisplayScaling;
