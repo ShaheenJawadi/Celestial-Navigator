fc8a0af63282eb3be1ddca193460ef6f09bdde73
shaheenj <jawadishaheen@gmail.com>
2024-10-21 05:30:27 +0100

    And add objects count component

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -2,10 +2,12 @@ import Dialog from "@/components/ui/dialog";
 import { Popup } from "@/components/ui/popup";
 import Drawer from "@/components/ui/drawer";
 import ToolsPanel from "@/components/toolsPanel";
+import ObjectsCount from "./objcetsCount";
 
 const UIPanels = () => {
   return (
     <>
+    <ObjectsCount/>
       <ToolsPanel />
       <Popup />
       <Dialog />
---
+++ src/components/ui/objcetsCount.tsx
@@ -0,0 +1,21 @@
+const ObjectsCount = () => {
+  return (
+    <div className="objectsCount">
+      <div>
+        <span> 🟢</span>
+        <span>Asteroid:500</span>
+      </div>
+      <div>
+        |<span>🔴</span>
+        <span>PHA:1000 </span>
+      </div>
+
+      <div> 
+        |<span>🟡</span>
+        <span>Comet:25</span>
+      </div>
+    </div>
+  );
+};
+
+export default ObjectsCount;
