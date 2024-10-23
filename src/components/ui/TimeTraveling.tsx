3fe6a571431fa5f3cd4a39e2d199ba01c1d0ffc7
shaheenj <jawadishaheen@gmail.com>
2024-10-23 19:39:58 +0100

    Refactor CSS and UI components for time traveling feature

--- src/components/ui/TimeTraveling.tsx
+++ src/components/ui/TimeTraveling.tsx
@@ -10,16 +10,21 @@ const TimeTraveling = () => {
           <div className="t_date">12/04/1998</div>
           <div className="t_time">17:20:15</div>
         </div>
-        <div className="btns">
-          <div className="singleBtn">
-            <Icon path={mdiRewind} size={1.5} />
-          </div>
-          <div className="singleBtn">
-            <Icon path={mdiPause} size={1.5} />
-          </div>
+        <div className="btm_i">
+{/*           <div className="steps">3 years /sec</div> */}
+          <div className="live"> <span></span> Live</div>
 
-          <div className="singleBtn">
-            <Icon path={mdiFastForward} size={1.5} />
+          <div className="btns">
+            <div className="singleBtn">
+              <Icon path={mdiRewind} size={1.5} />
+            </div>
+            <div className="singleBtn">
+              <Icon path={mdiPause} size={1.5} />
+            </div>
+
+            <div className="singleBtn">
+              <Icon path={mdiFastForward} size={1.5} />
+            </div>
           </div>
         </div>
       </div>
