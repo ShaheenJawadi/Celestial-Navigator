2a5bbed5b5b5128e2a7f98dc4c4e506eb8e3caa1
shaheenj <jawadishaheen@gmail.com>
2024-10-26 20:43:29 +0100

    Refactor searchObject component and update CSS selectors

--- src/components/toolsPanel/searchObject.tsx
+++ src/components/toolsPanel/searchObject.tsx
@@ -3,34 +3,90 @@ import Icon from "@mdi/react";
 import { SetStateAction, useState } from "react";
 
 const SearchObject = () => {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleInputChange = (e: { target: { value: SetStateAction<string>; }; }) => {
-      setSearchTerm(e.target.value);
-    };
-  
-    const handleSearch = (e: { preventDefault: () => void; }) => {
-      e.preventDefault();
-        console.log(searchTerm);
-    };
-    return (
-        <>
-        <form className={"searchForm"} onSubmit={handleSearch}>
+  const handleInputChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log(searchTerm);
+  };
+  return (
+    <div className="searchObject">
+      <form className={"searchForm"} onSubmit={handleSearch}>
         <input
           type="text"
           placeholder="Search for an object"
           value={searchTerm}
           onChange={handleInputChange}
-          className={'searchInput'}
+          className={"searchInput"}
         />
-        <button type="submit" className={"searchButton"}> 
+        <button type="submit" className={"searchButton"}>
           <Icon path={mdiMagnify} size={1} />
         </button>
-        
       </form>
-      <small>*Search for planet and NEO by Designation or SPK-ID or Object full name</small>
-      </>
-    );
-  };
-  export default SearchObject;
-  
+      <small>
+        *Search for planet and NEO by Designation or SPK-ID or Object full name
+      </small>
+
+      <div className="objList">
+        <div className="singleObj">
+          <div className="dataBox g">
+            <div className="single">
+              <h4>SPK-ID:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+            <div className="single">
+              <h4>Object fullname:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+            <div className="single">
+              <h4>Primary designation:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="singleObj PHA">
+          <div className="dataBox g">
+            <div className="single">
+              <h4>SPK-ID:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+            <div className="single">
+              <h4>Object fullname:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+            <div className="single">
+              <h4>Primary designation:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+          </div>
+        </div>
+        <div className="singleObj COMET">
+          <div className="dataBox g">
+            <div className="single">
+              <h4>SPK-ID:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+            <div className="single">
+              <h4>Object fullname:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+            <div className="single">
+              <h4>Primary designation:</h4>
+              <span>dsfdsdfsdfs</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+     
+    </div>
+  );
+};
+export default SearchObject;
